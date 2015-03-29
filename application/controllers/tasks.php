0bf55ae0ce2f48c110b4f48318e83f6e492ee695
Raja Sravan Sargu <rajasravan@gmail.com>
2015-03-29 18:56:15 +0530

    Get Tasks Procedure added

--- application/controllers/tasks.php
+++ application/controllers/tasks.php
@@ -9,36 +9,23 @@ class Tasks extends REST_Controller
 	// Get Tasks Data 
 	
 	function getTasks_get(){
-		//location
+		
+		if($this->get('taskid')){
+			$taskid = $this->get('taskid');
+			$queryString = "SELECT type AS task_type, contentid, advertiserid, validity, creation_date AS task_creation_date, last_update_date AS task_last_update_date  
+								FROM tasks T
+								WHERE id=".$taskid;
+			$query = $this->db->query($queryString);		
+		}
+		$response = array();
+		if ($query->num_rows() == 0) {
+			$response = array('message'=> 'No results');
+		} else{
+			$response = array('result' => $query->result());
+		}
+		$this->response($response, 200);
+	}
 
-		$this->db->from("tasks");
-		$this->db->order_by("name", "desc");
-		if($this->get('cityid')){
-			$cityid = $this->get('cityid');		
-			$this->db->where('cityid',$cityid);
-		}			
-		if($this->get('movieid')){
-			$movieid = $this->get('movieid');
-        	$this->db->where('`id` IN (SELECT `theatreid` FROM `theatres_movies` where movieid='.$movieid.')', NULL, FALSE);        	
-        	// SELECT distinct t.* FROM theatres_movies tm join theatres t on tm.theatreid = t.id where tm.movieid = 112 and t.cityid = 1
-        	$queryString = 'SELECT * from movies where id='.$movieid;			
-			$moviequery = $this->db->query($queryString);		
-		}
-		if($this->get('theatreId')){
-			$theatreid = $this->get('theatreId');
-        		$this->db->where('id', $theatreid);        	
-		}
-		$query = $this->db->get();
-        $response = array();
-        if ($query->num_rows() == 0) {
-                $response = array('message'=> 'No results');
-        } else if($this->get('movieid')){
-                $response = array('result' => $query->result(), 'movie'=>$moviequery->result());
-        } else{
-        		$response = array('result' => $query->result());
-        }
-        $this->response($response, 200);
-	}	
 	function getTaskDetails_get(){// get details and actions to be performned in a task given an id
 		//taskId, userid
 		Based on userid check has to be performed, whether user has any active tickets for this task already
